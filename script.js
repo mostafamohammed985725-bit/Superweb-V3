a17771b03981bb7d6a317988eca56060a83be552
mostafamohammed985725-bit <mostafamohammed985725@gmail.com>
2026-01-14 18:57:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ import { listenProjects } from "./firebase.js";
 
 let allProjects = [];
 
+// نربط الدالة بالـ window
+window.filterProjects = function(type) {
+  const filtered = allProjects.filter(p => p.type === type);
+  renderProjects(filtered);
+};
+
 listenProjects(projects => {
   allProjects = projects;
   renderProjects(allProjects);
 });
 
-window.filterProjects = function(type) {
-  const filtered = allProjects.filter(p => p.type === type);
-  renderProjects(filtered);
-}
-
 function renderProjects(list) {
   const box = document.getElementById("projects");
   box.innerHTML = "";
+
   list.forEach(p => {
     box.innerHTML += `
       <div class="card">
